f6b06e362bc6fd967ba962facc617dd136a494fa
jaanbaas <74250504+jaanbaas@users.noreply.github.com>
2023-03-16 17:54:13 +0530

    Add files via upload

---
+++ lib/test_screens/splash-1.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff0ca678),
+      body: Center(
+        child: Image.asset(
+          'assets/images/Splash-logo.gif',
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
